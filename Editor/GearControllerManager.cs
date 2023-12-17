b563ec34bbc0c27286fed9a2aa2e4cf69c92bf61
Yokesh <65059656+Yokesh-4040@users.noreply.github.com>
2023-12-18 00:53:07 +0530

    Added

--- Editor/GearControllerManager.cs
+++ Editor/GearControllerManager.cs
@@ -1,6 +1,58 @@
 using UnityEditor;
+using UnityEngine;
 
-public class GearControllerManager 
+namespace FourtyFourty.GearController.Editor
 {
+    [InitializeOnLoad]
+    public class GearControllerManager : MonoBehaviour
+    {
+        private const string PlayerPrefKey = "YourPlayerPrefKey";
+        private static bool _isPopUpShown;
+
+        static GearControllerManager()
+        {
+            // This code will be executed when Unity loads
+
+            if (PlayerPrefs.HasKey(PlayerPrefKey) || _isPopUpShown) return;
+            ShowPopup();
+            _isPopUpShown = true;
+        }
     
+        [MenuItem("Window/Show License Expiry Popup")]
+        private static void ShowLicenseExpiryPopup()
+        {
+            if (PlayerPrefs.HasKey(PlayerPrefKey) || _isPopUpShown) return;
+            ShowPopup();
+            _isPopUpShown = true;
+        }
+
+        private static void ShowPopup()
+        {
+            string message = "This project license is Expired. Please contact the developer";
+            string title = "License Expiry";
+
+            if (EditorUtility.DisplayDialog(title, message, "Close Tab", "Quit Unity Editor"))
+            {
+                // User clicked "Close Tab"
+                // Perform actions if needed
+            }
+            else
+            {
+                // User clicked "Quit Unity Editor"
+                EditorApplication.ExitPlaymode();
+                EditorApplication.isPlaying = false;
+                AssetDatabase.SaveAssets();
+                EditorApplication.Exit(0);
+            }
+        }
+    
+        private void OnLostFocus()
+        {
+            // This method is called when the window loses focus, such as when the close (X) button is clicked
+            EditorApplication.ExitPlaymode();
+            EditorApplication.isPlaying = false;
+            AssetDatabase.SaveAssets();
+            EditorApplication.Exit(0);
+        }
+    }
 }
