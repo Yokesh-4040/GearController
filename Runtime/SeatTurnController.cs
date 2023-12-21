c8e544a574c47575f427351e02e451fca90ca8e2
Yokesh <65059656+Yokesh-4040@users.noreply.github.com>
2023-12-21 11:41:33 +0530

    Update SeatTurnController.cs

--- Runtime/SeatTurnController.cs
+++ Runtime/SeatTurnController.cs
@@ -2,20 +2,28 @@ using UnityEngine;
 
 public class AngleBasedRotation : MonoBehaviour
 {
-    public float rotationSpeed = 5f; // Adjust the rotation speed as needed
-    public float minAngle = 45f; // Minimum angle for rotation to start
-    public float maxAngle = 135f; // Maximum angle for rotation to stop
-    
-    public Transform target;
+    public float rotationSpeed = 5f;  // Adjust the rotation speed as needed
+    public float minAngle = 45f;      // Minimum angle for rotation to start
+    public float maxAngle = 135f;     // Maximum angle for rotation to stop
 
-    void FixedUpdate()
+  
+
+    public Transform target;
+    public Vector3 myPos;
+    public Vector3 targetPos;
+
+    void Update()
     {
+
         // Assuming you want to rotate around the Y-axis
         float currentAngle = transform.eulerAngles.y;
+        targetPos = target.eulerAngles;
+        myPos = transform.eulerAngles;
+Debug.Log(target.localRotation.y);
         // Check if the current angle is between the specified range
         if (currentAngle > minAngle && currentAngle < maxAngle)
         {
-            if (target.eulerAngles.y <= 180)
+            if(target.eulerAngles.y<=180)
             {
                 // Rotate the object around its up axis (Y-axis)
                 target.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
@@ -23,8 +31,9 @@ public class AngleBasedRotation : MonoBehaviour
         }
         else
         {
-            if (target.localRotation.y > 0)
-                target.Rotate(Vector3.up, -rotationSpeed * Time.deltaTime);
+            if(target.localRotation.y>0)
+            target.Rotate(Vector3.up, -rotationSpeed * Time.deltaTime);
         }
+
     }
 }
