1e7ae6df1478986500351e46584682889ac1280a
Yokesh <65059656+Yokesh-4040@users.noreply.github.com>
2023-12-21 11:30:03 +0530

    update 1.1.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,10 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 Version Updated
 The version number for this package has increased due to a version update of a related graphics package.
 
+## [1.1.3] - 2023-12-20
+### Fixes
+- Added Seat Controller
+
 ## [1.1.2] - 2023-12-20
 ### Fixes
 - minor bug fix
--- Runtime/SeatTurnController.cs
+++ Runtime/SeatTurnController.cs
@@ -1,41 +1,30 @@
 using UnityEngine;
 
-public class SeatTurnController : MonoBehaviour
+public class AngleBasedRotation : MonoBehaviour
 {
+    public float rotationSpeed = 5f; // Adjust the rotation speed as needed
+    public float minAngle = 45f; // Minimum angle for rotation to start
+    public float maxAngle = 135f; // Maximum angle for rotation to stop
+    
+    public Transform target;
 
-    public Transform targetObject;
-    public float minLocalRotationAngle = 45f;
-    public float maxLocalRotationAngle = 135f;
-    public float timeThreshold = 5f;
-
-    private float timeWithinRange = 0f;
-    private bool hasRotated = false;
-
-    private void Update()
+    void FixedUpdate()
     {
-        // Get the current local rotation angle around the Z-axis
-        float currentLocalRotation = transform.localRotation.eulerAngles.y;
-
-        // Check if the current local rotation angle is between the specified range
-        if (currentLocalRotation > minLocalRotationAngle && currentLocalRotation < maxLocalRotationAngle)
+        // Assuming you want to rotate around the Y-axis
+        float currentAngle = transform.eulerAngles.y;
+        // Check if the current angle is between the specified range
+        if (currentAngle > minAngle && currentAngle < maxAngle)
         {
-            // Increment the timer
-            timeWithinRange += Time.deltaTime;
-
-            // Check if the user has been within the range for the specified duration
-            if (timeWithinRange >= timeThreshold && !hasRotated)
+            if (target.eulerAngles.y <= 180)
             {
-                // Rotate the target object locally by 180 degrees instantly
-                targetObject.localRotation *= Quaternion.Euler(0, 0, 180);
-                hasRotated = true;
+                // Rotate the object around its up axis (Y-axis)
+                target.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
             }
         }
         else
         {
-            // Reset the timer and rotation status if the user is not within the specified range
-            timeWithinRange = 0f;
-            hasRotated = false;
+            if (target.localRotation.y > 0)
+                target.Rotate(Vector3.up, -rotationSpeed * Time.deltaTime);
         }
     }
 }
-
