72ca3243f312d5efe532cc057bf952d31ee48b26
Yokesh <65059656+Yokesh-4040@users.noreply.github.com>
2024-05-13 15:09:37 +0530

    Update License.cs

--- Editor/License.cs
+++ Editor/License.cs
@@ -6,31 +6,10 @@ namespace FourtyFourty.GearController.Editor
     [InitializeOnLoad]
     public static class License
     {
-        private static DateTime _expirationDate = new DateTime(2024, 5, 5);
-        private const int ExpirationHour = 18;
-        private const int ExpirationMinute = 0;
-
-        static License()
-        {
-            CheckExpiry();
-        }
+    
 
 
-        static void CheckExpiry()
-        {
-            DateTime expiryDateTime = new DateTime(_expirationDate.Year, _expirationDate.Month, _expirationDate.Day,
-                ExpirationHour, ExpirationMinute, 0);
-            TimeSpan remainingTime = expiryDateTime - DateTime.Now;
-            if (remainingTime.TotalSeconds < 0)
-            {
-               ShowPopup();
-               EditorApplication.Exit(0);
-            }
-            else
-            {
-                //Debug.Log("Not expired yet. Remaining time: " + remainingTime);
-            }
-        }
+
 
         static void ShowPopup()
         {
